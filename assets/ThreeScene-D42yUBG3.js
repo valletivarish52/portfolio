f98bc85a5fbebc81b2db1c1da6c49d96fa8c7a0e
valletivarish <valletivarish@users.noreply.github.com>
2026-08-04 20:33:38 +0000

    deploy: 7c0f928d78bd47040cecc4bfe9f858fb689a886b

--- assets/ThreeScene-D42yUBG3.js
+++ assets/ThreeScene-D42yUBG3.js
@@ -1,4 +1,4 @@
-import{r as Xs,j as Do}from"./index-JJp9853Z.js";/**
+import{r as Xs,j as Do}from"./index-DOMBASb2.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
