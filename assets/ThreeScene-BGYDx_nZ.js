99be33073578cb31e032f89e08765a48798b7640
valletivarish <valletivarish@users.noreply.github.com>
2026-08-09 15:00:46 +0000

    deploy: a9eb7363e45366bf87e18badfdd1bcf2cfc644d4

--- assets/ThreeScene-BGYDx_nZ.js
+++ assets/ThreeScene-BGYDx_nZ.js
@@ -1,4 +1,4 @@
-import{r as Xs,j as Lo}from"./index-DosaDWR-.js";/**
+import{r as Xs,j as Lo}from"./index-BJrkU25R.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
