3c34aed5197275904e1a56d1979bc43f9551280f
valletivarish <valletivarish@users.noreply.github.com>
2026-08-09 14:26:20 +0000

    deploy: b6a98152c5ec97df3451164d26b69446bff2223d

--- assets/ThreeScene-BXUre1xx.js
+++ assets/ThreeScene-BXUre1xx.js
@@ -1,4 +1,4 @@
-import{r as Xs,j as Lo}from"./index-CQSaUpC8.js";/**
+import{r as Xs,j as Lo}from"./index-CGiDPLCL.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
