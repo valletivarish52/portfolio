40d2991129308c6d790d69d5f1fce5103bc5f9af
valletivarish <valletivarish@users.noreply.github.com>
2026-08-11 09:34:37 +0000

    deploy: cfc0ca7c05f3fa315f4696ad09c1dca33f127b83

--- assets/ThreeScene-Cv5FEUaC.js
+++ assets/ThreeScene-Cv5FEUaC.js
@@ -1,4 +1,4 @@
-import{r as Xs,j as Lo}from"./index-BJrkU25R.js";/**
+import{r as Xs,j as Lo}from"./index-Hw1dx98x.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
